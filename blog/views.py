cc02bf54af88169defb9792ae6673dd5b622fd6c
MyintMyatKaungPHP <myintmyatkaung.info@gmail.com>
2019-02-07 02:49:07 +0630

    Django Tutorial part 5

---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+posts = [
+    {
+        'author': 'Myint Myat Kaung',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 11, 2018'
+    },
+    {
+        'author': 'Mg Kaung',
+        'title': 'Blog Post 2',
+        'content': 'Secont post content',
+        'date_posted': 'August 21, 2018'
+    },
+]
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all(),
+        'title': 'Home',
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html',context)
